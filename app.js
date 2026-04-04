f17f4b266ce3914812b87dc645c5665ecccae3bb
Abdulwakeel Kesington <abdulwakeelkesington@gmail.com>
2026-04-04 15:22:37 +0100

    refactor: comment out unused middleware and server creation for clarity

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const express = require("express");
 
 const app = express()
 
-app.use((req, res, next) => {
-    console.log("in the middleware");
-    next();
+// app.use((req, res, next) => {
+//     console.log("in the middleware");
+//     next();
+// })
+
+app.use('/add-product', (req, res, next) => {
+    console.log("in another middleware");
+    res.send("<h1>The 'Add Product' page</h1>");
 })
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
     console.log("in another middleware");
     res.send("<h1>Hello from Express</h1>")
 })
-
 // const server = http.createServer(app);
 
 
